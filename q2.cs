b19340243bfe1b4f94af6dca21a9b91943c5b645
Hirai0827 <41163700+Hirai0827@users.noreply.github.com>
2019-01-29 15:59:03 +0900

    init
    initialize the repository

---
+++ q2.cs
@@ -0,0 +1,37 @@
+﻿namespace OpenCvSharp
+{
+    using UnityEngine;
+    using System.Collections;
+    using OpenCvSharp;
+    using UnityEngine.UI;
+    using System;
+    public class q2 : MonoBehaviour
+    {
+        public Texture2D texture;
+        // Use this for initialization
+        void Start()
+        {
+            Mat mat = Unity.TextureToMat(this.texture);
+            for(int yi = 0; yi < mat.Height; yi++)
+            {
+                for(int xi = 0; xi < mat.Width; xi++)
+                {
+                    Vec3b v = mat.At<Vec3b>(yi,xi);
+                    float gr = 0.2126f * v[2] + 0.7152f * v[1] + 0.0722f * v[0];
+                   v[0] = (byte)gr;
+                    v[1] = (byte)gr;
+                    v[2] = (byte)gr;
+                    mat.Set<Vec3b>(yi, xi, v);
+                }
+            }
+            Texture2D changedTex = Unity.MatToTexture(mat);
+            GetComponent<RawImage>().texture = changedTex;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
